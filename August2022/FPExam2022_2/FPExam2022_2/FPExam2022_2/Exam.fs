f4d00e7cbd04b7f746e3d0e8961b7ca3d2fd4cec
Philip Mørch <mrphil2105@gmail.com>
2023-05-31 13:10:24 +0200

    Solutions question 2 August 2022

--- August2022/FPExam2022_2/FPExam2022_2/FPExam2022_2/Exam.fs
+++ August2022/FPExam2022_2/FPExam2022_2/FPExam2022_2/Exam.fs
@@ -2,18 +2,18 @@
 (* If you are importing this into F# interactive then comment out
    the line above and remove the comment for the line bellow.
 
-   Do note that the project will not compile if you do this, but 
-   it does allow you to work in interactive mode and you can just remove the '=' 
+   Do note that the project will not compile if you do this, but
+   it does allow you to work in interactive mode and you can just remove the '='
    to make the project compile again.
 
    Do not remove the module declaration (even though that does work) because you may inadvertently
    introduce indentation errors in your code that may be hard to find if you want
-   to switch back to project mode. 
+   to switch back to project mode.
 
    Alternative, keep the module declaration as is, but load ExamInteractive.fsx into the interactive environment
    *)
 (*
- module Exam2022_2 = 
+ module Exam2022_2 =
  *)
 
 (* 1: Grayscale images *)
@@ -21,25 +21,25 @@
     type grayscale =
     | Square of uint8
     | Quad of grayscale * grayscale * grayscale * grayscale
-    
-    let img = 
-      Quad (Square 255uy, 
-            Square 128uy, 
-            Quad(Square 255uy, 
-                 Square 128uy, 
+
+    let img =
+      Quad (Square 255uy,
+            Square 128uy,
+            Quad(Square 255uy,
+                 Square 128uy,
                  Square 192uy,
                  Square 64uy),
             Square 0uy)
-    
+
 (* Question 1.1 *)
     let maxDepth _ = failwith "not implemented"
-    
+
 (* Question 1.2 *)
     let mirror _ = failwith "not implemented"
 
 (* Question 1.3 *)
     let operate _ = failwith "not implemented"
-    
+
     let mirror2 _ = failwith "not implemented"
 
 (* Question 1.4 *)
@@ -52,7 +52,7 @@
         | []               -> []
         | x :: xs when f x -> x :: (foo f xs)
         | _ :: xs          -> foo f xs
-            
+
     let rec bar fs xs =
         match fs with
         | []       -> xs
@@ -60,43 +60,53 @@
 
 (* Question 2.1 *)
 
-    (* 
-    
+    (*
+
     Q: What are the types of functions foo and bar?
 
     A: <Your answer goes here>
 
 
-    Q: What do the functions foo and  bar do. 
+    Q: What do the functions foo and  bar do.
        Focus on what it does rather than how it does it.
 
     A: <Your answer goes here>
-    
-    Q: What would be appropriate names for functions 
+
+    Q: What would be appropriate names for functions
        foo and bar?
 
     A: <Your answer goes here>
-        
-    Q: The function foo uses an underscore `_` in its third case. 
+
+    Q: The function foo uses an underscore `_` in its third case.
        Is this good coding practice, if so why, and if not why not?
-    
+
     A: <Your answer goes here>
     *)
-        
+
 
 (* Question 2.2 *)
 
-    let bar2 _ = failwith "not implemented"
+    let bar2 fs xs =
+        List.fold (fun xs f ->
+            List.filter f xs
+        ) xs fs
 
-(* Question 2.3 *) 
+(* Question 2.3 *)
 
-    let baz _ = failwith "not implemented"
+    let baz fs =
+        fun x ->
+            let rec aux =
+                function
+                | [] -> true
+                | f :: fs when f x -> aux fs
+                | _ -> false
+            aux fs
 
 (* Question 2.4 *)
 
     (*
 
-    Q: Only one of the functions `foo` and `bar` is tail recursive. Which one? 
+    Q: Only one of the functions `foo` and `bar` is tail recursive. Which one?
        Demonstrate why the other one is not tail recursive.
        To make a compelling argument you should evaluate a function call of the function,
        similarly to what is done in Chapter 1.4 of HR, and reason about that evaluation.
@@ -104,12 +114,29 @@
        Keep in mind that all steps in an evaluation chain must evaluate to the same value
        ((5 + 4) * 3 --> 9 * 3 --> 27, for instance).
 
-    A: <Your answer goes here>
+       let rec foo f =
+            function
+            | []               -> []
+            | x :: xs when f x -> x :: (foo f xs)
+            | _ :: xs          -> foo f xs
+
+    A: Foo is not tail recursive, since 'x :: (foo f xs)' depends on the return value of the foo call.
+
+       Function evaluation:
+
+       --- goes here ---
 
     *)
 (* Question 2.5 *)
     // only implement the one that is NOT already tail recursive
-    let fooTail _ = failwith "not implemented"
+    let fooTail f xs =
+        let rec aux xs c =
+            match xs with
+            | [] -> c []
+            | x :: xs when f x -> aux xs (fun r -> c (x :: r))
+            | _ :: xs -> aux xs c
+        aux xs id
+
     let barTail _ = failwith "not implemented"
 
 
@@ -129,12 +156,12 @@
     let randomOracle _ = failwith "not implemented"
 
 (* Question 3.3 *)
-    
+
     let findNumber _ = failwith "not implemented"
 
 (* Question 3.4 *)
     let evilOracle _ = failwith "not implemented"
-    
+
 (* Question 3.5 *)
     let parFindNumber _ = failwith "not implemented"
 
@@ -148,8 +175,8 @@
     | MULT of register * register * register
     | SUB of register * register * register
     | JGTZ of register * address
-    
-     
+
+
     let factorial x =           // Address
         [MOVI (R1, 1)           // 0
          MOVI (R2, x)           // 1
@@ -157,7 +184,7 @@
          MULT (R1, R1, R2)      // 3 (Loop starts here)
          SUB  (R2, R2, R3)      // 4
          JGTZ (R2, 3u)]         // 5 (Loop ends here)
-    
+
 (* Question 4.1 *)
 
     type program = unit (* replace this entire type with your own *)
@@ -167,27 +194,27 @@
 
     type state = unit (* replace this entire type with your own *)
     let emptyState _ = failwith "not implemented"
-    
+
 
 (* Question 4.3 *)
 
     let setRegister _ = failwith "not implemented"
-    
+
     let getRegister _ = failwith "not implemented"
-    
+
     let setProgramCounter _ = failwith "not implemented"
-    
+
     let getProgramCounter _ = failwith "not implemented"
-    
+
     let getProgram _ = failwith "not implemented"
-    
+
 (* Question 4.4 *)
-    
+
     type StateMonad<'a> = SM of (state -> 'a * state)
 
     let ret x = SM (fun s -> x, s)
-    let bind f (SM a) : StateMonad<'b> = 
-      SM (fun s -> 
+    let bind f (SM a) : StateMonad<'b> =
+      SM (fun s ->
       let x, s' = a s
       let (SM g) = f x
       g s')
@@ -198,15 +225,15 @@
     let evalSM prog (SM f) = f (emptyState prog)
 
     let setReg _ = failwith "not implemented"
-    
+
     let getReg _ = failwith "not implemented"
-    
+
     let setPC _ = failwith "not implemented"
-    
+
     let incPC _ = failwith "not implemented"
-    
+
     let lookupCmd _ = failwith "not implemented"
-    
+
 
 
 (* Question 4.5 *)
@@ -221,4 +248,3 @@
     let state = new StateBuilder()
 
     let runProgram _ = failwith "not implemented"
-    
